73a43c5fbe16ae1e2bbe6da1a2b105e9dbfabcc9
AsmirH12 <94566949+AsmirH12@users.noreply.github.com>
2023-10-29 16:13:30 -0400

    done first test correction

--- pages/part-a.js
+++ pages/part-a.js
@@ -16,29 +16,36 @@ export default function section1() {
           <h2 className={styles.test}>Test 1</h2>
           <div>
             <h3>Section III, Question 3</h3>
-            <p>Determine how many atoms of <MathJax.Node inline formula={"H"} /> are in one mouthful of water if the average mouth can hold <MathJax.Node inline formula={"84.2g"} /> of water.</p>
+            <p><b>Determine how many atoms of <MathJax.Node inline formula={"H"} /> are in one mouthful of water if the average mouth can hold <MathJax.Node inline formula={"84.2 \\ g"} /> of water.</b></p>
+            <p>I mistakenly assumed that <MathJax.Node inline formula={"84.2 \\ g"}/> was the mass of <MathJax.Node inline formula={"H"}/>, when it is actually the mass of <MathJax.Node inline formula={"H_{2}O"}/>. Corrected solution:</p>
+            
             <div className={styles.columns}>
               <p>
-               <MathJax.Node formula={"M_{H}=84.2g"} />
-
-                <MathJax.Node formula={"M_{Fe}=5\frac{g}{mol}"} />
-                <MathJax.Node formula={"M_{Fe}=5\frac{g}{mol}"} />
+               <MathJax.Node formula={"m_{H_{2}O}=84.2 \\ g"} />
+                <MathJax.Node formula={"M_{H_{2}O}=18.02 \\ \\frac{g}{mol}"} />
+                <MathJax.Node formula={"N_{A}=\\frac{6.02 \\times 10^{23}}{mol}"} />
+                <MathJax.Node formula={"N_{H}= \\ ?"} />
               </p>
 
               <p>
-                <MathJax.Node inline formula={"M_{Fe}=5\frac{g}{mol}"} />
+                <MathJax.Node formula={"n=\\frac{m}{M}"} />
+                <MathJax.Node formula={"N=n \\times N_{A}"} />
+                <MathJax.Node formula={"N=\\frac{m}{M} \\times N_{A}"} />
               </p>
 
           
               <div>
               
-                <MathJax.Node formula={"M_{Fe}=5\frac{g}{mol}"} />
-                <MathJax.Node formula={"M_{Fe}=5\frac{g}{mol}"} />
-                <MathJax.Node formula={"M_{Fe}=5\frac{g}{mol}"} />
-                <MathJax.Node formula={"M_{Fe}=5\frac{g}{mol}"} />
-                <MathJax.Node formula={"=5g"} />
-                <MathJax.Node formula={"=53249324g/mol888"} />
-              
+                <MathJax.Node formula={"N_{H_{2}O}=\\frac{m_{H_{2}O}}{M_{H_{2}O}}(N_{A})"} />
+                <MathJax.Node formula={"=\\frac{84.2 \\  g}{1.01 \\ \\frac{g}{mol}}(\\frac{6.02  \\times 10^{23} \\ molecules}{mol})"} />
+                <MathJax.Node formula={"=\\frac{84.2 \\ mol(6.02 \\times 10^{23} \\ molecules)}{1.01 \\ mol}"} />
+                <MathJax.Node formula={"=\\frac{84.2(6.02 \\times 10^{23})}{1.01} \\ molecules"}/>
+                <MathJax.Node formula={"=502 \\times 10^{23} \\ molecules"}/>
+                <MathJax.Node formula={"N_{H_{2}O}=5.02 \\times 10^{25} molecules"}/>
+                <MathJax.Node formula={""}/>
+                <MathJax.Node formula={"N_{H}=2(N_{H_{2}O}) \\ atoms"}/>
+                <MathJax.Node formula={"=10.04 \\times 10^{25} \\ atoms"}/>
+                <MathJax.Node formula={"N_{H}=1.00 \\times 10^{26} \\ atoms"}/>
               </div>
               
               
